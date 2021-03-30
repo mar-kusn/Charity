5251e54ad2d9e397365205904835a02db2d1f282
Mariusz <mar-kus@o2.pl>
2021-03-30 22:40:29 +0200

    Institution entity + repository created

---
+++ src/main/java/pl/coderslab/institution/InstitutionRepository.java
@@ -0,0 +1,6 @@
+package pl.coderslab.institution;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InstitutionRepository extends JpaRepository<Institution, Long> {
+}
